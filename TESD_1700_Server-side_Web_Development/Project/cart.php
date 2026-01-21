90269de5d7b3aece8732afe55b96253d2428d8be
Miles-Wiser <129561396+Miles-Wiser@users.noreply.github.com>
2026-01-21 14:14:19 -0700

    Add files via upload

---
+++ TESD_1700_Server-side_Web_Development/Project/cart.php
@@ -0,0 +1,16 @@
+<?php
+require "page.php";
+
+$page = new Page();
+$page->title = "$page->title | Cart";
+$page->moreCSS = "cart.css";
+
+$page->DisplayHead();
+
+?>
+<main>
+    
+</main>
+
+<?php
+$page->DisplayFooter();
---
+++ TESD_1700_Server-side_Web_Development/Project/index.php
@@ -0,0 +1,39 @@
+<?php
+require "page.php";
+
+$page = new Page();
+
+$page->DisplayHead();
+
+?>
+
+<main>
+    <a href="catalog.html" id="main-shop" class="section-preview">
+        <div>
+            <h2>Professional Quality</h2>
+            <h3>Fit For Every Level</h3>
+            <p>Shop Now</p>
+        </div>
+    </a>
+    <a href="#" id="main-horns" class="section-preview">
+        <div>
+            <h2>Beginner Horns</h2>
+            <p>Browse high quality and versitile beginner friendly horns.</p>
+        </div>
+    </a>
+    <a href="#" id="main-woodwinds" class="section-preview">
+        <div>
+            <h2>Woodwinds</h2>
+            <p>View our new woodwind selection from Amati to Yamaha</p>
+        </div>
+    </a>
+    <a href="#" id="main-electric" class="section-preview">
+        <div>
+            <h2>Electric Equipment</h2>
+            <p>Elevate your sounds with our line of amps and electric gear.</p>
+        </div>
+    </a>
+</main>
+
+<?php
+$page->DisplayFooter();
